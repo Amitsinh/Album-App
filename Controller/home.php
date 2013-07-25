114838f78c1524fa997b46c2a93abd7cbe2447b5
Amitsinh <amitsinh.chavda@yahoo.com>
2013-07-25 03:31:41 -0700

    Update home.php

--- Controller/home.php
+++ Controller/home.php
@@ -1,7 +1,6 @@
 <?php
     require 'lib/facebook-php-sdk-master/src/facebook.php';
     require 'picasa.php';
-    require 'PHPMailer/class.phpmailer.php';
 
 
 	class home{
@@ -77,58 +76,6 @@
             {
                 echo "<a href='".$photos['photos']['data'][$i]['source']."'class='fancybox' data-fancybox-group='gallery' title='".$photos['photos']['data'][$i]['id']."'></a>";
             }
-        }
-
-        // Sent Email With Zip link
-
-        function email()
-        {
-            error_reporting(E_ERROR);
-
-            $message = '<html>
-
-                            <body>
-                                <div style="border:1px solid #ccc;">
-                                    <div style="padding:20px;text-size:16px;color:#fff;background:#2BA6CB;border-bottom:1px solid #ccc;">Album App</div>
-                                    <div style="padding:20px; color:#666666;">
-                                        <p><b>Hello '.$_SESSION['user_name'].',</b></p><br />
-                                        <p>Thank you for using our service.</p>
-					      <p>To Download Your zipped album,Click on <a href=http://50.112.48.91/'.$_SESSION['fileName'].'>http://50.112.48.91/'.$_SESSION['fileName'].'</a></p><br/>
-                                        <p>Album App <br>Director </p>
-                                    <div>
-                                </div>
-                            </body>
-                            </html>';
-
-              $subject='Album Zip Link';
-              $to =$_SESSION['email'];
-
-		$mail = new PHPMailer;
-
-		$mail->IsSMTP();                                      // Set mailer to use SMTP
-		$mail->Host = 'smtp.gmail.com';  // Specify main and backup server
-		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'EmailId';                            // SMTP username
-		$mail->Password = 'Password';                           // SMTP password
-		$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
-		
-		$mail->From = 'Notification@AlbumApp.com';
-		$mail->FromName = 'AlbumApp Notification';
-		$mail->AddAddress($_SESSION['email'], $_SESSION['user_name']);  // Add a recipient
-		$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-		$mail->IsHTML(true);                                  // Set email format to HTML
-		$mail->Subject = 'Zip Link';
-		$mail->Body    = $message;
-		
-
-		if(!$mail->Send()) {
-		    echo 'Message could not be sent.';
-		    echo 'Mailer Error: ' . $mail->ErrorInfo;
-		    exit;
-		} 
-           
-            
-            echo $_SESSION['email'];
         }
 
         //Logout from Facebook & Album App
